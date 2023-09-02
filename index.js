7d2aed42c824c6358cfd508a9f131b05680b263e
Paulo Santiago <pauloecsantiago@gmail.com>
2023-09-02 10:41:31 +0100

    Change class id search-film-btn

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-//Get the template document
-var w = window.innerWidth;
+// @ts-nocheck
+/* var w = window.innerWidth;
 var h = window.innerHeight;
 
 console.log(h);
+ */
+
+const btnSearch = document.getElementById("btn").value;
+
+console.log(btnSearch);
+
+/* btnSearch.addEventListener(function (e) {
+  e.preventDefault();
+  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=34c3217c`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+});
+ */
