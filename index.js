2ddb4c65b446c8062b4a75f14ae0e28370ff53a6
Paulo Santiago <pauloecsantiago@gmail.com>
2023-09-04 16:58:31 +0100

    Rendering film working with one film only

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ Please append it to all of your API requests,
 OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=34c3217c */
 
   /*   const response = fetch(`https://www.omdbapi.com/?t=${title}&apikey=34c3217c`); */
-  const response = fetch(`https://www.omdbapi.com/?s=${title}&apikey=34c3217c`);
+  const response = fetch(`https://www.omdbapi.com/?t=${title}&apikey=34c3217c`);
 
   const movie = (await response).json();
   const film = await movie;
@@ -32,100 +32,51 @@ OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=34c3217c */
   /* console.log(film) */
   /* console.log(film.Search) */
 
-  const filmSearch = film.Search;
+  /*   const filmSearch = film.Search; */
 
   filmContainer.innerHTML = "";
-
-  for (let title in filmSearch) {
-    filmContainer.innerHTML += ` 
-
-     <!-- Image film -->
-     <div class="film-item" id="film-item">
-        <div class="img-wrapper">
-          <img class="img-film" src="${filmSearch[title].Poster}" alt="" /> 
-        </div>         
-
-<div class="all-features">
-
-  <div class="info-wrapper">
-    <!-- title -->
-    <div class="div-title">
-      <h2>${filmSearch[title].Title}</h2>  
-      <img src="images/icon_star.svg" alt="rate" /> 
-      <p class="rate">${film.imdbRating}</p> 
-    </div>
-    <!-- info -->
-    <div class="div-info">
-      <p>${filmSearch[title].Runtime}</p>   
-      <p>${filmSearch[title].Genre}</p> 
-      <button class="btn-list">
-        <img
-          class="btn-list"
-          src="images/icon_plus.svg"
-          alt=""
-        />Watchlist
-      </button>
-    </div>
-    <!-- Description -->
-    <div class="div-description">
-      <p class="p-description">  
-       ${filmSearch[title].Plot}
-      </p>
-    </div>
-
-    </div>
-
-`;
-    console.log(filmSearch[title].Title);
-  }
-
-  /*   for(let title in film.Search) {
-    
-    
-      filmContainer.innerHTML += ` <!-- FILM INFO -->   
-
-
-<!-- Image film -->
-<div class="img-wrapper">
-  <img class="img-film" src="${film.Search[Poster]}" alt="" /> 
-</div>         
-
-
-<div class="all-features">
-
-  <div class="info-wrapper">
-    <!-- title -->
-    <div class="div-title">
-      <h2>${film.Title}</h2>  
-      <img src="images/icon_star.svg" alt="rate" /> 
-      <p class="rate">${film.imdbRating}</p> 
-    </div>
-    <!-- info -->
-    <div class="div-info">
-      <p>${film.Runtime}</p>   
-      <p>${film.Genre}</p> 
-      <button class="btn-list">
-        <img
-          class="btn-list"
-          src="images/icon_plus.svg"
-          alt=""
-        />Watchlist
-      </button>
-    </div>
-    <!-- Description -->
-    <div class="div-description">
-      <p class="p-description">  
-       ${film.Plot}
-      </p>
-    </div>
-
-`;
-  
-    console.log(film.Search[title])
-  }  */
+  getFilm(film);
 });
 
-/* function render() {
-  filmContainer = "";
+function getFilm(film) {
+  filmContainer.innerHTML = ` 
+
+  <!-- Image film -->
+  <div class="film-item" id="film-item">
+     <div class="img-wrapper">
+       <img class="img-film" src="${film.Poster}" alt="" /> 
+     </div>         
+
+<div class="all-features">
+
+<div class="info-wrapper">
+ <!-- title -->
+ <div class="div-title">
+   <h2>${film.Title}</h2>  
+   <img src="images/icon_star.svg" alt="rate" /> 
+   <p class="rate">${film.imdbRating}</p> 
+ </div>
+ <!-- info -->
+ <div class="div-info">
+   <p>${film.Runtime}</p>   
+   <p>${film.Genre}</p> 
+   <button class="btn-list">
+     <img
+       class="btn-list"
+       src="images/icon_plus.svg"
+       alt=""
+     />Watchlist
+   </button>
+ </div>
+ <!-- Description -->
+ <div class="div-description">
+   <p class="p-description">  
+    ${film.Plot}
+   </p>
+ </div>
+
+ </div>
+
+`;
+  return filmHtml;
 }
- */
