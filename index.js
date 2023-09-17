fa808f3ff41957b23f25d0909d8d6f2a2d182d8b
Paulo Santiago <pauloecsantiago@gmail.com>
2023-09-17 12:26:58 +0100

    film search

--- index.js
+++ index.js
@@ -23,60 +23,127 @@ Please append it to all of your API requests,
 
 OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=34c3217c */
 
-  /*   const response = fetch(`https://www.omdbapi.com/?t=${title}&apikey=34c3217c`); */
-  const response = fetch(`https://www.omdbapi.com/?t=${title}&apikey=34c3217c`);
+  /* const response = fetch(`https://www.omdbapi.com/?t=${title}&apikey=34c3217c`);   */
+  const response = fetch(
+    `https://www.omdbapi.com/?s=${title}&t=${title}&apikey=34c3217c`
+  );
 
   const movie = (await response).json();
   const film = await movie;
+  /*    console.log(film.Search)  */
+  filmContainer.innerHTML = "";
+
+  for (let title of film.Search) {
+    getFilm(title);
+    /*  console.log(title.Title) */
+  }
   /* http://www.omdbapi.com/?s=Batman&page=2 */
-  /* console.log(film) */
+  /*   console.log(film.Search.imdbID)  */
   /* console.log(film.Search) */
 
   /*   const filmSearch = film.Search; */
 
-  filmContainer.innerHTML = "";
-  getFilm(film);
+  /* if(inputTitle.value) {
+    filmContainer = '' */
+
+  /* } */
 });
 
-function getFilm(film) {
-  filmContainer.innerHTML = ` 
+/* Function to place film  */
+function getFilm(filmInput) {
+  console.log(filmInput);
 
-  <!-- Image film -->
-  <div class="film-item" id="film-item">
-     <div class="img-wrapper">
-       <img class="img-film" src="${film.Poster}" alt="" /> 
-     </div>         
+  filmContainer.innerHTML += `  
+   <div class="film-container" id="film-container">
+     <!-- Image film -->
+     <div class="film-item" id="film-item">
+        <div class="img-wrapper">
+          <img class="img-film" src="${filmInput.Poster}" alt="" /> 
+        </div>         
 
 <div class="all-features">
 
-<div class="info-wrapper">
- <!-- title -->
- <div class="div-title">
-   <h2>${film.Title}</h2>  
-   <img src="images/icon_star.svg" alt="rate" /> 
-   <p class="rate">${film.imdbRating}</p> 
- </div>
- <!-- info -->
- <div class="div-info">
-   <p>${film.Runtime}</p>   
-   <p>${film.Genre}</p> 
-   <button class="btn-list">
-     <img
-       class="btn-list"
-       src="images/icon_plus.svg"
-       alt=""
-     />Watchlist
-   </button>
- </div>
- <!-- Description -->
- <div class="div-description">
-   <p class="p-description">  
-    ${film.Plot}
-   </p>
- </div>
+  <div class="info-wrapper">
+    <!-- title -->
+    <div class="div-title">
+      <h2>${filmInput.Title}</h2>  
+      <img src="images/icon_star.svg" alt="rate" /> 
+      <p class="rate">${filmInput.imdbRating}</p> 
+    </div>
+    <!-- info -->
+    <div class="div-info">
+      <p>${filmInput.Runtime}</p>   
+      <p>${filmInput.Genre}</p> 
+      <button class="btn-list">
+        <img
+          class="btn-list"
+          src="images/icon_plus.svg"
+          alt=""
+        />Watchlist
+      </button>
+    </div>
+    <!-- Description -->
+    <div class="div-description">
+      <p class="p-description">  
+       ${filmInput.Plot}
+      </p>
+    </div>
 
- </div>
+    </div>
+    
+    </div>
 
 `;
-  return filmHtml;
 }
+
+/* Render film */
+
+/*  function render() {
+  
+  filmContainer.innerHTML = getFilm()
+  
+} */
+
+/* LOOP WITH FILM SEARCH */
+/*    for (let title in filmSearch) {
+    filmContainer.innerHTML += ` 
+
+     <!-- Image film -->
+     <div class="film-item" id="film-item">
+        <div class="img-wrapper">
+          <img class="img-film" src="${filmSearch[title].Poster}" alt="" /> 
+        </div>         
+
+<div class="all-features">
+
+  <div class="info-wrapper">
+    <!-- title -->
+    <div class="div-title">
+      <h2>${filmSearch[title].Title}</h2>  
+      <img src="images/icon_star.svg" alt="rate" /> 
+      <p class="rate">${film.imdbRating}</p> 
+    </div>
+    <!-- info -->
+    <div class="div-info">
+      <p>${filmSearch[title].Runtime}</p>   
+      <p>${filmSearch[title].Genre}</p> 
+      <button class="btn-list">
+        <img
+          class="btn-list"
+          src="images/icon_plus.svg"
+          alt=""
+        />Watchlist
+      </button>
+    </div>
+    <!-- Description -->
+    <div class="div-description">
+      <p class="p-description">  
+       ${filmSearch[title].Plot}
+      </p>
+    </div>
+
+    </div>
+
+`;
+    console.log(filmSearch[title].Title);
+  }  */
